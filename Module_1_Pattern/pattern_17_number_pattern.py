11cd3c21be80691471e919f630adf339f6a99602
Ayush Sisodiya <xerbias2026@gmail.com>
2026-05-20 00:50:04 +0530

    contineous sequence number pattern

--- Module_1_Pattern/pattern_17_number_pattern.py
+++ Module_1_Pattern/pattern_17_number_pattern.py
@@ -5,11 +5,27 @@
 7 8 9 10
 11 12 13 14 15
 
-'''
+
 n = 5
 y = 1
 for i in range(1,n+1,1):
     for j in range(1,i+1,1):
         print(y,end=' ')
         y = y+1
+    print('')
+'''
+'''
+15 
+14 13 
+12 11 10
+9  8  7  6
+5  4  3  2  1
+
+'''
+n = 5
+y = 15
+for i in range(1,n+1,1):
+    for j in range(1,i+1,1):
+        print(y,end=' ')
+        y-=1
     print('')
