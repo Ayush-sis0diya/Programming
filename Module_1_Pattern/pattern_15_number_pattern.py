e3b574f28c8f536196256add37266592ecafe9a0
Ayush Sisodiya <xerbias2026@gmail.com>
2026-05-20 00:23:23 +0530

    extra variable method

--- Module_1_Pattern/pattern_15_number_pattern.py
+++ Module_1_Pattern/pattern_15_number_pattern.py
@@ -5,8 +5,20 @@
 1 2 3 4
 1 2 3 4 5
 '''
+
+'''
 n = 5
 for i in range(1,n+1,1):
     for j in range(1,i+1,1):
         print(j,end='')
+    print('')
+'''
+
+#using extra variable
+n = 5
+for i in range(1,n+1,1):
+    y = 1
+    for j in range(1,i+1,1):
+        print(y,end = '')
+        y = y+1
     print('')
