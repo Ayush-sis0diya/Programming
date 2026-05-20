5fb474a2da14b0d4dc28bbd9b31b8a80c4731b5a
Ayush Sisodiya <xerbias2026@gmail.com>
2026-05-20 12:53:27 +0530

    advance number sequence patten

--- Module_1_Pattern/pattern_18_Advance_number_pattern.py
+++ Module_1_Pattern/pattern_18_Advance_number_pattern.py
@@ -5,7 +5,7 @@
 -1234321          -****+++
 123454321         *****++++
 '''
-
+'''
 n=5
 
 for i in range(1,n+1,1):
@@ -19,4 +19,28 @@ for i in range(1,n+1,1):
     for j in range(1,i-1+1,1):
         print(y,end='')
         y-=1
+    print('')
+
+'''
+'''
+12345 4321
+-1234 321
+--123 21
+---12 1
+----1 
+
+'''
+n= 5
+for i in range(1,n+1,1):
+    for j in range(1,i-1+1,1):
+        print('-',end='')
+    y=1
+    for j in range(1,n-i+1+1,1):
+        print(y,end='')
+        y+=1
+
+    y=n-i
+    for j in range(1,n-i+1,1):
+        print(y,end='')
+        y=y-1
     print('')
