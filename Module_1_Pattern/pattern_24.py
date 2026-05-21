844f9268e2e22a068dd70d801702723d2beb730c
Ayush Sisodiya <xerbias2026@gmail.com>
2026-05-22 01:24:45 +0530

    update

--- Module_1_Pattern/pattern_24.py
+++ Module_1_Pattern/pattern_24.py
@@ -3,7 +3,7 @@
 -****
 --****
 ---****
-'''
+
 n = 4
 for i in range(1,n+1,1):
     for j in range(1,i-1+1,1):
@@ -11,3 +11,7 @@ for i in range(1,n+1,1):
     for j in range(1,n+1,1):
         print('*',end = '')
     print('')
+'''
+
+
+                       
