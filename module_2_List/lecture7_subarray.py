43dff9a94ed85dcf840a489f4992c07f40d019c5
Ayush Sisodiya <xerbias2026@gmail.com>
2026-05-29 23:25:23 +0530

    subarray count

--- module_2_List/lecture7_subarray.py
+++ module_2_List/lecture7_subarray.py
@@ -11,7 +11,7 @@ for i in range(len(a)):
 
 
 #sum of all the sub array 
-
+'''
 for i in range(len(a)):
     for j in range(i,len(a)):
         sum = 0
@@ -20,3 +20,18 @@ for i in range(len(a)):
             sum = sum+a[k]
         print("sum =",sum)
         print('')
+'''
+
+# count all the sub array with sum = k
+
+count = 0
+target = 30
+for i in range(len(a)):
+    for j in range(i,len(a)):
+        sum = 0
+        for k in range(i,j+1):
+            sum = sum+a[k]
+        if sum == target:
+            count = count+1
+        print('')
+print("count of sub array of sum is :",count) 
