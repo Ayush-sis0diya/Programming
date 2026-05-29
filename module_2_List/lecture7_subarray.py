d30f2a61ababe29127efa90527523547c1499d2e
Ayush Sisodiya <xerbias2026@gmail.com>
2026-05-29 22:39:54 +0530

    sum of all sub array

--- module_2_List/lecture7_subarray.py
+++ module_2_List/lecture7_subarray.py
@@ -1,9 +1,22 @@
 #all sub array of an main array using brute force
 
 a = [10,20,30,40,50,60]
-
+'''
 for i in range(len(a)):
     for j in range(i,len(a)):
         for k in range(i,j+1):
             print(a[k],end = ' ')
+        print('')
+'''
+
+
+#sum of all the sub array 
+
+for i in range(len(a)):
+    for j in range(i,len(a)):
+        sum = 0
+        for k in range(i,j+1): 
+            print(a[k],end = ' ')
+            sum = sum+a[k]
+        print("sum =",sum)
         print('')
