e9dd2a547ce0987c3f385ddc49846524fad24893
Ayush Sisodiya <xerbias2026@gmail.com>
2026-05-29 22:18:07 +0530

    all subarray of an single element

---
+++ module_2_List/lecture7_subarray.py
@@ -0,0 +1,9 @@
+#all sub array of an main array using brute force
+
+a = [10,20,30,40,50,60]
+
+for i in range(len(a)):
+    for j in range(i,len(a)):
+        for k in range(i,j+1):
+            print(a[k],end = ' ')
+        print('')
